413527ba33031f5328ecc68e2676326c83f702ba
devsjc <devsjc@users.noreply.github.com>
2023-01-18 17:05:05 +0000

    Add generated files [ci skip]

--- gen/pvsite.sql
+++ gen/pvsite.sql
@@ -1,6 +1,6 @@
 -- SQL dump generated using DBML (dbml-lang.org)
 -- Database: PostgreSQL
--- Generated at: 2023-01-18T17:02:25.558Z
+-- Generated at: 2023-01-18T17:04:39.757Z
 
 CREATE TABLE "sites" (
   "site_uuid" uuid PRIMARY KEY NOT NULL,
