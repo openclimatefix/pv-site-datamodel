e576ac95f2d9145955d5d1bd5a2c229c7fce21be
devsjc <47188100+devsjc@users.noreply.github.com>
2023-01-24 15:35:09 +0000

    Update workflow

--- sdk/python/pvsite_datamodel/write/__init__.py
+++ sdk/python/pvsite_datamodel/write/__init__.py
@@ -2,5 +2,5 @@
 Write functions for PVSite db
 """
 
-from forecast import insert_forecast_values
-from generation import insert_generation_values
+from .forecast import insert_forecast_values
+from .generation import insert_generation_values
