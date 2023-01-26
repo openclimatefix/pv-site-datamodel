b4f5d23150227342ba8e4b8ef49bf51f6d88cd16
peterdudfield <peter.dudfield@hotmail.com>
2023-01-26 14:22:16 +0000

    add long description back in

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -1,3 +1,10 @@
+from pathlib import Path
 from setuptools import setup
 
-setup()
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
