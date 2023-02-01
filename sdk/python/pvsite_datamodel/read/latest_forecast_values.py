6c16ccdc2d0471c02d9a3e96e311b185dc50656a
Peter Dudfield <34686298+peterdudfield@users.noreply.github.com>
2023-02-01 17:44:36 +0000

    Update read (#36)
    * make read function more flexible

--- sdk/python/pvsite_datamodel/read/latest_forecast_values.py
+++ sdk/python/pvsite_datamodel/read/latest_forecast_values.py
@@ -10,17 +10,21 @@ from pvsite_datamodel.sqlmodels import DatetimeIntervalSQL, LatestForecastValueS
 
 
 def get_latest_forecast_values_by_site(
-    session: Session, site_uuids: List[uuid.UUID], start_utc: Optional[dt.datetime] = None
+    session: Session,
+    site_uuids: List[uuid.UUID],
+    start_utc: Optional[dt.datetime] = None,
+    model=LatestForecastValueSQL,
 ) -> Dict[uuid.UUID, List[LatestForecastValueSQL]]:
     """Get the latest forecast values by input sites.
 
     :param session: The sqlalchemy database session
     :param site_uuids: list of site_uuids for which to fetch latest forecast values
     :param start_utc: filters on forecast values target_time >= start_utc
+    :param model, the database table to use, could be 'LatestForecastValueSQL' or ForecastValueSQL
     :return: dict containing {site_uuid1: List[LatestForecastValueSQL], site_uuid2: ...}
     """
     # start main query
-    query: Query = session.query(LatestForecastValueSQL)
+    query: Query = session.query(model)
     query = query.join(DatetimeIntervalSQL)
 
     if start_utc is not None:
@@ -28,24 +32,24 @@ def get_latest_forecast_values_by_site(
 
         # also filter on creation time, to speed up things
         created_utc_filter = start_utc - dt.timedelta(days=1)
-        query = query.filter(LatestForecastValueSQL.created_utc >= created_utc_filter)
+        query = query.filter(model.created_utc >= created_utc_filter)
 
-    output_dict: Dict[uuid.UUID, List[LatestForecastValueSQL]] = {}
+    output_dict: Dict[uuid.UUID, List[model]] = {}
 
     # Filter the query on the desired sites
-    query = query.where(LatestForecastValueSQL.site_uuid.in_(site_uuids))
+    query = query.where(model.site_uuid.in_(site_uuids))
 
     # order by site, target time and created time desc
     query.order_by(
-        LatestForecastValueSQL.site_uuid,
+        model.site_uuid,
         DatetimeIntervalSQL.start_utc,
-        LatestForecastValueSQL.created_utc,
+        model.created_utc,
     )
 
-    latest_forecast_values: List[LatestForecastValueSQL] = query.all()
+    latest_forecast_values: List[model] = query.all()
 
     for site_uuid in site_uuids:
-        site_latest_forecast_values: List[LatestForecastValueSQL] = [
+        site_latest_forecast_values: List[model] = [
             lfv for lfv in latest_forecast_values if lfv.site_uuid == site_uuid
         ]
 
