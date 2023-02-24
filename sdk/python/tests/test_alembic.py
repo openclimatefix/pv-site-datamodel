05ccbedb5694a96cb39c6167851f4682362ef12d
Simon Lemieux <1105380+simlmx@users.noreply.github.com>
2023-02-24 05:26:12 +0000

    Test the migrations
    Add a test that runs all our migrations on a clean database.

---
+++ sdk/python/tests/test_alembic.py
@@ -0,0 +1,15 @@
+import os
+import subprocess
+
+from testcontainers.postgres import PostgresContainer
+
+
+def test_migrations():
+    """Run the alembic migrations on a clean database."""
+    with PostgresContainer("postgres:14.5") as postgres:
+        url = postgres.get_connection_url()
+
+        env = os.environ.copy()
+        env["DB_URL"] = url
+        result = subprocess.run(["alembic", "upgrade", "head"], env=env)
+        assert result.returncode == 0
