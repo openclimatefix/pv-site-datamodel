7de0a190b006cf391441fc7680053d1ea3272b1b
Andrew Lester <alester220@gmail.com>
2023-04-20 15:27:16 -0500

    Add new Inverters related data models (#68)

--- pvsite_datamodel/__init__.py
+++ pvsite_datamodel/__init__.py
@@ -1,6 +1,14 @@
 """Python SDK for reading/writing to/from pvsite database."""
 
 from .connection import DatabaseConnection
-from .sqlmodels import ClientSQL, ForecastSQL, ForecastValueSQL, GenerationSQL, SiteSQL, StatusSQL
+from .sqlmodels import (
+    ClientSQL,
+    ForecastSQL,
+    ForecastValueSQL,
+    GenerationSQL,
+    InverterSQL,
+    SiteSQL,
+    StatusSQL,
+)
 
 __version__ = "0.1.31"
