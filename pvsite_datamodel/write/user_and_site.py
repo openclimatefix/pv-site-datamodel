c0956f7c4bfbb9de5ae0ad216d1d064b501ff98c
Rachel Tipton <rachelmtipton@gmail.com>
2023-10-18 12:34:09 +0200

    add create user and create site functions

--- pvsite_datamodel/write/user_and_site.py
+++ pvsite_datamodel/write/user_and_site.py
@@ -51,6 +51,117 @@ def make_user(db_session, email, site_group):
 
     return user
 
+# create functions for site and user
+def create_new_site(
+    session: Session,
+    client_site_id: int,
+    client_site_name: str,
+    latitude: float,
+    longitude: float,
+    capacity_kw: float,
+    dno: Optional[str] = None,
+    gsp: Optional[str] = None,
+    region: Optional[str] = None,
+    orientation: Optional[float] = None,
+    tilt: Optional[float] = None,
+    inverter_capacity_kw: Optional[float] = None,
+    module_capacity_kw: Optional[float] = None,
+) -> SiteSQL:
+    """Creates a site and adds it to the database.
+    This is used in the OCF Dashboard.
+    :param session: database session
+    :param client_site_id: id the client uses for the site
+    :param client_site_name: name the client uses for the site
+    :param latitude: latitude of site as an integer
+    :param longitude: longitude of site as an integer
+    :param capacity_kw: capacity of site in kw
+    :param dno: dno of site
+    :param gsp: gsp of site
+    :param region: region of site, deafut is uk
+    :param orientation: orientation of site, default is 180
+    :param tilt: tilt of site, default is 35
+    :param inverter_capacity_kw: inverter capacity of site in kw
+    :param module_capacity_kw: module capacity of site in kw
+
+    """
+    max_ml_id = session.query(func.max(SiteSQL.ml_id)).scalar()
+
+    if max_ml_id is None:
+        max_ml_id = 0
+
+    if region in [None, ""]:
+        region = "uk"
+
+    if orientation in [None, ""]:
+        orientation = 180
+
+    if tilt in [None, ""]:
+        tilt = 35
+
+    if inverter_capacity_kw in [None, ""]:
+        inverter_capacity_kw = capacity_kw
+
+    if module_capacity_kw in [None, ""]:
+        module_capacity_kw = capacity_kw
+
+    if gsp is None:
+        gsp = get_gsp(latitude=latitude, longitude=longitude)
+        gsp = json.dumps(gsp)
+
+    if dno is None:
+        dno = get_dno(latitude=latitude, longitude=longitude)
+        dno = json.dumps(dno)
+
+    site = SiteSQL(
+        ml_id=max_ml_id + 1,
+        client_site_id=client_site_id,
+        client_site_name=client_site_name,
+        latitude=latitude,
+        longitude=longitude,
+        capacity_kw=capacity_kw,
+        dno=dno,
+        gsp=gsp,
+        region=region,
+        orientation=orientation,
+        tilt=tilt,
+        inverter_capacity_kw=inverter_capacity_kw,
+        module_capacity_kw=module_capacity_kw,
+    )
+
+    session.add(site)
+
+    session.commit()
+
+    message = f"Site with client site id {site.client_site_id} and site uuid {site.site_uuid} created successfully"
+
+    return site, message
+
+
+def create_user(
+    session: Session,
+    email: str,
+    site_group_name: str,
+) -> UserSQL:
+    """Create a user.
+    :param session: database session
+    :param email: email of user being created
+    :param site_group: name of the site group this user will be part of
+    """
+
+    site_group = (
+        session.query(SiteGroupSQL)
+        .filter(SiteGroupSQL.site_group_name == site_group_name)
+        .first()
+    )
+
+    user = UserSQL(email=email, site_group_uuid=site_group.site_group_uuid)
+
+    session.add(user)
+    site_group.users.append(user)
+    session.commit()
+
+    return user
+
 # delete functions for site, user, and site group
 def delete_site(session: Session, site_uuid: str) -> SiteGroupSQL:
     """Delete a site group.
