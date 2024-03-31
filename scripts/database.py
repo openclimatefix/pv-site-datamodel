f537e1ac2a3c4ce772f2cabc2a2d7aef4fcd6b40
Suleman Karigar <37236131+suleman1412@users.noreply.github.com>
2024-03-31 19:29:59 +0530

    Add function to save API request to database

---
+++ scripts/database.py
@@ -0,0 +1,29 @@
+import structlog
+from pvsite_datamodel.read.user import get_user_by_email as get_user_by_db
+from pvsite_datamodel.sqlmodels import APIRequestSQL
+logger = structlog.stdlib.get_logger()
+def save_api_call_to_db(request, session, user=None):
+    """
+    Save api call to database
+
+    If the user does not have an email address, we will save the email as unknown
+    :param request:
+    :return:
+    """
+
+    url = str(request.url)
+
+    if user is None:
+        email = "unknown"
+    else:
+        email = user.email
+
+    # get user from db
+    user = get_user_by_db(session=session, email=email)
+    # make api call
+    logger.info(f"Saving api call ({url=}) to database for user {email}")
+    api_request = APIRequestSQL(url=url, user=user)
+
+    # commit to database
+    session.add(api_request)
+    session.commit()
