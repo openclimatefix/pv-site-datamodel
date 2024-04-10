1d816b6e3378826fd9d83316d12003dd5ad21574
Suleman Karigar <37236131+suleman1412@users.noreply.github.com>
2024-04-10 17:48:57 +0530

    solved linting issues

--- pvsite_datamodel/write/database.py
+++ pvsite_datamodel/write/database.py
@@ -1,16 +1,18 @@
+"""
+Functions to read from the database and format.
+"""
 import logging
+
 from pvsite_datamodel.read.user import get_user_by_email as get_user_by_db
 from pvsite_datamodel.sqlmodels import APIRequestSQL
+
 logger = logging.getLogger(__name__)
+
+
 def save_api_call_to_db(url, session, user=None):
     """
-    Save api call to database
-
-    If the user does not have an email address, we will save the email as unknown
-    :param request:
-    :return:
+    Save api call to database.
     """
-
     url = str(url)
     if user is None:
         email = "unknown"
