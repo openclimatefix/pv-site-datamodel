9bb199c8aa6de0001fab6540a1a9e200dac74bff
peterdudfield <peter.dudfield@hotmail.com>
2024-08-15 21:53:42 +0100

    read models names

--- tests/read/test_read_models.py
+++ tests/read/test_read_models.py
@@ -1,7 +1,9 @@
 """ test get models"""
+import pandas as pd
 
-from pvsite_datamodel.read.model import get_or_create_model
+from pvsite_datamodel.read.model import get_models, get_or_create_model
 from pvsite_datamodel.sqlmodels import MLModelSQL
+from pvsite_datamodel.write.forecast import insert_forecast_values
 
 
 def test_get_model(db_session):
@@ -15,3 +17,39 @@ def test_get_model(db_session):
 
     _ = get_or_create_model(session=db_session, name="test_name", version="9.9.10")
     assert len(db_session.query(MLModelSQL).all()) == 2
+
+
+def test_get_models(db_session):
+    _ = get_or_create_model(session=db_session, name="test_name", version="9.9.9")
+    _ = get_or_create_model(session=db_session, name="test_name", version="9.9.10")
+
+    models = get_models(session=db_session)
+
+    assert len(models) == 1
+
+
+def test_get_models_with_datetimes(db_session):
+    model = get_or_create_model(session=db_session, name="test_name", version="9.9.10")
+
+    df = pd.DataFrame(
+        {
+            "site_uuid": ["test_uuid"],
+            "start_utc": [pd.Timestamp("2021-01-01 00:00:00")],
+            "power_kw": [0.0],
+        }
+    )
+    forecast_meta = {
+        "site_uuid": "test_uuid",
+        "timestamp_utc": pd.Timestamp("2021-01-01 00:00:00"),
+        "forecast_version": "0.0.0",
+    }
+
+    insert_forecast_values(
+        session=db_session,
+        forecast_meta=forecast_meta,
+        forecast_values_df=df,
+        ml_model_name=model.name,
+        ml_model_version=model.version,
+    )
+
+    _ = get_models(session=db_session, start_datetime=pd.Timestamp("2021-01-01 00:00:00"))
