3e07e7360de2149182a63b45f8cf0d26e1392e45
PrabhasKalyan <prabhaskalyan0473@gmail.com>
2024-11-25 02:15:08 +0530

    Add a filter on api read #298
    Add a filter that can only get or exclude strings when reading API users

--- pvsite_datamodel/sqlmodels.py
+++ pvsite_datamodel/sqlmodels.py
@@ -173,6 +173,8 @@ class SiteSQL(Base, CreatedMixin):
         comment="Auto-incrementing integer ID of the site for use in ML training",
     )
 
+    active = sa.Column(sa.Boolean,unique=False,default=True)
+
     client_uuid = sa.Column(
         UUID(as_uuid=True),
         sa.ForeignKey("clients.client_uuid"),
