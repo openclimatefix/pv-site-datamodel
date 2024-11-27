00bbefeeebcdc115a0eb515433f19df223851e11
PrabhasKalyan <prabhaskalyan0473@gmail.com>
2024-11-27 22:25:48 +0530

    add tests

--- tests/read/test_get_api_requests.py
+++ tests/read/test_get_api_requests.py
@@ -50,3 +50,17 @@ def test_get_api_requests_for_one_user_end_datetime(db_session):
         end_datetime=dt.datetime.now(tz=dt.timezone.utc) - dt.timedelta(hours=1),
     )
     assert len(requests_sql) == 0
+
+
+def test_get_api_requests_for_one_user_new(db_session):
+    user = get_user_by_email(session=db_session, email="test@test.com")
+    db_session.add(APIRequestSQL(user_uuid=user.user_uuid, url="test"))
+
+    requests_sql = get_api_requests_for_one_user(
+        session=db_session,
+        email=user.email,
+        include_in_url="API",
+        exclude_in_url="UI",
+    )
+    assert len(requests_sql) == 1
+    assert requests_sql[0].url == "test"
