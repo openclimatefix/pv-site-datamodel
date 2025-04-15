9d5f2cf012ba290eaea5ea551445f0828ab059cb
Dakshbir <144359831+Dakshbir@users.noreply.github.com>
2025-04-16 00:15:03 +0530

    Update env.py

--- alembic/env.py
+++ alembic/env.py
@@ -1,12 +1,10 @@
-"""Alembic's env.py file."""
-
 import os
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config, pool
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
 
 from alembic import context
-from pvsite_datamodel.sqlmodels import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -14,13 +12,13 @@ config = context.config
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
-if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
+fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
+from pvsite_datamodel.sqlmodels import Base
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
@@ -28,13 +26,8 @@ target_metadata = Base.metadata
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
 
-# Use the URL from alembic.ini rather than environment variable
-url = config.get_main_option("sqlalchemy.url")
-if not url:
-    raise ValueError("No database URL configured in alembic.ini")
 
-
-def run_migrations_offline() -> None:
+def run_migrations_offline():
     """Run migrations in 'offline' mode.
 
     This configures the context with just a URL
@@ -51,7 +44,6 @@ def run_migrations_offline() -> None:
         url=url,
         target_metadata=target_metadata,
         literal_binds=True,
-        compare_server_default=True,
         dialect_opts={"paramstyle": "named"},
     )
 
@@ -59,7 +51,7 @@ def run_migrations_offline() -> None:
         context.run_migrations()
 
 
-def run_migrations_online() -> None:
+def run_migrations_online():
     """Run migrations in 'online' mode.
 
     In this scenario we need to create an Engine
@@ -67,13 +59,15 @@ def run_migrations_online() -> None:
 
     """
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
+        config.get_section(config.config_ini_section),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
 
     with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata,compare_server_default=True)  # compare_server_default=True is used to compare the server default values in the database with the ones in the model
+        context.configure(
+            connection=connection, target_metadata=target_metadata
+        )
 
         with context.begin_transaction():
             context.run_migrations()
