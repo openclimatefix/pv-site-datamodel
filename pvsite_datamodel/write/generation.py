2143d6efff63dcad9a6955ec9716db0da60ae76e
Peter Dudfield <34686298+peterdudfield@users.noreply.github.com>
2025-07-12 10:14:19 +0100

    update write method for generation (#228)
    * update write method for generation
    
    * refactor

--- pvsite_datamodel/write/generation.py
+++ pvsite_datamodel/write/generation.py
@@ -23,9 +23,15 @@ def insert_generation_values(
     :param session: sqlalchemy session for interacting with the database
     :param df: dataframe with the data to insert
     """
+
+    # rename site_uuid to location_uuid
+    if "site_uuid" in df.columns and "location_uuid" not in df.columns:
+        df.rename(columns={"site_uuid": "location_uuid"}, inplace=True)
+
     # Check for duplicated (site_uuid, start_utc) entries.
     # TODO Should we have a unique constraint on those in the Database instead?
-    sites_with_duplicate_times = df[df.duplicated(["site_uuid", "start_utc"])]["site_uuid"].unique()
+    duplicates = df[df.duplicated(["location_uuid", "start_utc"])]
+    sites_with_duplicate_times = duplicates["location_uuid"].unique()
     if len(sites_with_duplicate_times) > 0:
         for site_uuid in sites_with_duplicate_times:
             _log.warning(f'duplicate target datetimes for site "{site_uuid}"')
@@ -34,14 +40,14 @@ def insert_generation_values(
     generation_sqls: list[dict] = []
 
     for _, row in df.iterrows():
-        site_uuid = row["site_uuid"]
+        location_uuid = row["location_uuid"]
         start_utc = row["start_utc"]
         power_kw = row["power_kw"]
         end_utc = row.get("end_utc", default=start_utc + dt.timedelta(minutes=5))
 
         # Create a GenerationSQL object for each generation, and surface as dict
         generation = GenerationSQL(
-            location_uuid=site_uuid,
+            location_uuid=location_uuid,
             generation_power_kw=power_kw,
             start_utc=start_utc,
             # TODO This is arbitrary and should be fixed
